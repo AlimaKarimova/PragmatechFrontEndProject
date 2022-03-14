ec0ecde5b6a9e1b7b1f53b041d99a6fb27629ea7
AlimaKarimova <way.s@inbox.ru>
2022-03-14 20:02:20 +0400

    2 js tapsirigi yazildi

---
+++ JStasks/app.js
@@ -0,0 +1,25 @@
+// Görüləcək işlər
+
+// 1. a b və c ədədləri verilir. a və b ədədlərindən hansının c ədədinə daha yaxın olduğunu tapmağınız tələb olunur.
+
+// 2.  x adında dəyişən təyin edin. bu dəyişənə istənilən tipdə data təyin edilə bilər.Sizdən tələb olununan daxil edilən dəyərin növünü tapmaq və ona uyğun ekrana mesaj yazdırmaqdır. (Nümunə: Daxil etdiyini dəyər rəqəmdir)
+// prompt vasitesile daxil olunan her deyer string qebul olunur deye bu usulla yazdim.
+// let x= null;
+// let netice= typeof x;
+// alert(`daxil etdiyiniz deyerin data tipi ${netice}`)
+
+// 3. verilən x ədədinin rəqəmlərini ekrana çap edən proqram yazın
+// let x = 625;
+// let y = String(x);
+// for (let i = 0; i < y.length; i++) {
+//     console.log(y[i])
+// }
+
+// 4. verilən 4 ədədi böyükdən kiçiyə doğru ekrana çap edən program yazın
+
+// 5. verilən kəsirli ədədin (nümumə: 4.234) tam hissəsi ilə kəsirli hissəsini ayrı ayrı ekrana çap edən program yazın
+
+// 6. 3 nöqtənin koordinatları verilir.Bu verilən koordinatlara görə a nöqtəsinə ən yaxın nöqtəni tapan proqram yazın
+// a_x=120,a_y=200
+// b_x=230,b_y=400
+// c_x=210,c_y=345
