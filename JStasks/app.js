dcf64e983a05baa19a2b56f18ad18eacd5ff114b
AlimaKarimova <way.s@inbox.ru>
2022-03-15 23:20:24 +0400

    4 tapsiriq yazildi

--- JStasks/app.js
+++ JStasks/app.js
@@ -1,6 +1,21 @@
 // Görüləcək işlər
 
 // 1. a b və c ədədləri verilir. a və b ədədlərindən hansının c ədədinə daha yaxın olduğunu tapmağınız tələb olunur.
+let a=10;
+let b=20;
+let c=15;
+if (c!==a && c!==b){
+    if (c>a && c>b){
+        if(a>b){
+            console.log('a c-ye daha yaxindir')
+        }
+        else {
+            console.log('b c-ye daha yaxindir')
+        }
+    }
+    else if()
+    
+}
 
 // 2.  x adında dəyişən təyin edin. bu dəyişənə istənilən tipdə data təyin edilə bilər.Sizdən tələb olununan daxil edilən dəyərin növünü tapmaq və ona uyğun ekrana mesaj yazdırmaqdır. (Nümunə: Daxil etdiyini dəyər rəqəmdir)
 // prompt vasitesile daxil olunan her deyer string qebul olunur deye bu usulla yazdim.
@@ -17,7 +32,18 @@
 
 // 4. verilən 4 ədədi böyükdən kiçiyə doğru ekrana çap edən program yazın
 
+// const Numbers= [65, 3, 84, 12];
+// let AzalanSira= Numbers.sort(function(a, b){return b-a});
+// console.log(AzalanSira)
+
+
 // 5. verilən kəsirli ədədin (nümumə: 4.234) tam hissəsi ilə kəsirli hissəsini ayrı ayrı ekrana çap edən program yazın
+
+// let x = 4.234;
+// x= String(x)
+// let y = x.split(".");
+// alert(y[0])
+// alert(y[1])
 
 // 6. 3 nöqtənin koordinatları verilir.Bu verilən koordinatlara görə a nöqtəsinə ən yaxın nöqtəni tapan proqram yazın
 // a_x=120,a_y=200
