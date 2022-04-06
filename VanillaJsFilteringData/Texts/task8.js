4c1fcc573cc4dbed9e625c6edd341d02d40149a1
AlimaKarimova <way.s@inbox.ru>
2022-04-06 22:54:16 +0400

    Tasklarin bir hissesi yazildi

--- VanillaJsFilteringData/Texts/task8.js
+++ VanillaJsFilteringData/Texts/task8.js
@@ -2,26 +2,7 @@ const sampleNews = `As software developers, the natural tendency is to start dev
 The obvious answer could be derived from the application’s design. Keeping the system design as clean and scalable as possible is one of the critical things that any professional developer should dedicate their time to. And that’s where SOLID design principles come into play. It helps developers eliminate design smells and build the best designs for a set of features.
 Although the SOLID design principles were first introduced by the famous Computer Scientist Robert C. Martin (a.k.a. Uncle Bob) in his paper in 2000, its acronym was introduced later by Michael Feathers. Uncle Bob is also the author of best-selling books Clean Code, Clean Architecture, Agile Software Development: Principles, Patterns, and Practices.`
 
-// Mətndəki boşluq sayını return edən funksiya yazın
-// write function that return amount of spaces in string
-// const bosluq=sampleNews.match(/ /g)
-// console.log(bosluq.length)
 
-// Mətndəki söz sayını return edən funksiya yazın
-// write function that return word count
-// const sozSayi=sampleNews.split(" ")
-// console.log(sozSayi.length)
 
-// sampleNews mətninə əsasən verilən tapşırıqları yerinə yetirin
-// Mətndəki xarakter sayını return edən funksiya yazın
-// write function that return character count of string 
-// Mətndəki cümlə sayını return edən funksiya yazın
-// write function that return amount of sentences
-// Mətndəki hərf sayını return edən funksiya yazın
-// write function that return letter count
-// Mətndə olan bütün böyük hərflərin olduğu arrayı return edən funksiya yazın
-// write function that return array of Capitalized words in string
 // Mətn daxilindən uzunluğu 40-50 xarakter arasında olan təsadüfi mətni return edən funksiya yazın
 // Seçilən mətn uzunluğu sözü bölürsə o sözü silməlisiniz.Məsələn seçilən mətnin sonu developers sözünün deve hissəsində bitirsə developers sözü silinməlidir
-// Mətn daxilində böyük hərflə başlayan sözlərin olduğu arrayı return edən funksiya yazın
-// write function that return array words that has Capital letter
