d2cac4bcc9ea09fa95c9710b20400d7a7fdf4416
AlimaKarimova <way.s@inbox.ru>
2022-05-06 18:26:08 +0400

    .

--- Tasks_6-13/TagSelectorSingleSelector/app.js
+++ Tasks_6-13/TagSelectorSingleSelector/app.js
@@ -1,21 +1,23 @@
 let btns = document.querySelectorAll('.btn');
 let icon = document.createElement('i');
 icon.classList = "fa-regular fa-circle-check";
-
+commonList = document.querySelector('.buttons-container');
 
 
 function btnChecked(e) {
     for (let btn of btns) {
         e.target.appendChild(icon);
+
     }
 };
 
 function changeColor(e) {
     for (let btn of btns) {
-        e.target.classList.add('checked');
-        btn.classList.remove('checked')
-        // axrincida sehv var
-
+        if (commonList.closest('checked')) {
+            btn.classList.remove('checked')
+        } else {
+            e.target.classList.add('checked')
+        }
     }
 };
 
