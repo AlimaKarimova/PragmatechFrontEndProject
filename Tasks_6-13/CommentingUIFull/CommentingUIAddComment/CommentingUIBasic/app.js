dcf825dfb402453f28e7c4e7ad58ee0f746823e3
AlimaKarimova <way.s@inbox.ru>
2022-05-09 18:22:28 +0400

    .

---
+++ Tasks_6-13/CommentingUIFull/CommentingUIAddComment/CommentingUIBasic/app.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('.input'),
+commentBox = document.querySelector('.comments-list'),
+form = document.querySelector('form'),
+commentsCount = document.querySelector('.comments-count span');
+let count = 1;
+
+
+
+input.addEventListener('change', function (e) {
+    const comment = document.createElement("div");
+    comment.className = 'comment';
+    const commentText = input.value;
+    input.value = ' ';
+    comment.innerHTML = `<div class="comment-image">
+    <img src="./user1.png" alt=""></div>
+    <div class="comment-info">
+        <span class="name">Kenzo Nakamura</span>
+        <span class="date">18days ago</span>
+        <p class="serial-number">0x2a0d29...269BBb6</p>
+        <p>${commentText}</p>
+    </div>`;
+    count++;
+    commentsCount.innerHTML = `${count} comments`
+    commentBox.prepend(comment);
+    e.preventDefault()
+});
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
