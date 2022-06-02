7a0974772f16045ac186bbb1db1cae23a6386dc1
AlimaKarimova <way.s@inbox.ru>
2022-06-03 00:28:58 +0400

    products-info

--- eCommerceAppWithFakeApi/js/index.js
+++ eCommerceAppWithFakeApi/js/index.js
@@ -87,3 +87,38 @@ output.innerHTML = slider.value;
 slider.oninput = function () {
   output.innerHTML = `$ ${this.value}`;
 }
+
+// function hideAllParagraphs() {
+//   for (let item of allItems) {
+//       // p.style.display = "none"
+//       item.querySelector("p").style.display = "none"
+//   }
+// }
+
+// function Accordion() {
+
+//   hideAllParagraphs()
+//   accordionData.forEach(function(element, index) {
+
+//       accContainer.innerHTML += GenerateItemUI(element.acc_title, element.acc_body)
+//   })
+// }
+
+
+// function AccFunc(e) {
+//   hideAllParagraphs()
+//   if (acar) {
+//       e.nextElementSibling.style.display = "none"
+//       e.querySelector('i').className = "fa-solid fa-chevron-up"
+//           // e.querySelector('i').style.transform = "rotate(-180deg)"
+
+//       acar = false
+//   } else {
+//       e.nextElementSibling.style.display = "block"
+//       e.querySelector('i').className = "fa-solid fa-chevron-down"
+//           // e.querySelector('i').style.transform = "rotate(0deg)"
+//       acar = true
+//   }
+// }
+
+// Accordion()
